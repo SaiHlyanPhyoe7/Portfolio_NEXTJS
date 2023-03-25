edaf7a3eabae2cd869a884ab84b0398847095088
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2023-03-25 23:14:51 +0630

    feature/add eslint and folder structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,36 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['plugin:react/recommended', 'prettier', 'standard-with-typescript', 'next/core-web-vitals'],
+  extends: [
+    'plugin:react/recommended',
+    'prettier',
+    'standard-with-typescript',
+    'next/core-web-vitals'
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: ['./tsconfig.json']
   },
-  plugins: ['react'],
+  plugins: ['react', 'unused-imports'],
   rules: {
+    // '@typescript-eslint/semi': ['error'],
+    'comma-dangle': ['off', 'never'],
+    'no-tabs': ['error', { allowIndentationTabs: true }],
     '@typescript-eslint/space-before-function-paren': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off'
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_'
+      }
+    ]
   },
   settings: {
     react: {
