fb2a5f47582687e0556ef005104614f677db5969
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2023-04-08 00:56:11 +0630

    add cursor follower

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,34 +4,34 @@ import { Box, MantineProvider } from '@mantine/core'
 import { Layout } from '@/layout/Layout'
 import * as React from 'react'
 import { Notifications } from '@mantine/notifications'
-// import useMeasure from 'react-use-measure'
-// import { useTrail, animated } from '@react-spring/web'
+import useMeasure from 'react-use-measure'
+import { useTrail, animated } from '@react-spring/web'
 
-// import styles from './styles.module.css'
-// import dynamic from 'next/dynamic'
+import styles from './styles.module.css'
+import dynamic from 'next/dynamic'
 
-// const fast = { tension: 1200, friction: 40 }
-// const slow = { mass: 10, tension: 200, friction: 50 }
-// const trans = (x: number, y: number) =>
-//   `translate3d(${x}px,${y}px,0) translate3d(-50%,-50%,0)`
+const fast = { tension: 1200, friction: 40 }
+const slow = { mass: 10, tension: 200, friction: 50 }
+const trans = (x: number, y: number) =>
+  `translate3d(${x}px,${y}px,0) translate3d(-50%,-50%,0)`
 
-// const AnimatedCursor = dynamic(
-//   async () => await import('react-animated-cursor'),
-//   {
-//     ssr: false
-//   }
-// )
+const AnimatedCursor = dynamic(
+  async () => await import('react-animated-cursor'),
+  {
+    ssr: false
+  }
+)
 
 export default function App({ Component, pageProps }: AppProps) {
-  // const [trail, api] = useTrail(3, (i) => ({
-  //   xy: [0, 0],
-  //   config: i === 0 ? fast : slow
-  // }))
-  // const [ref, { left, top }] = useMeasure()
+  const [trail, api] = useTrail(3, (i) => ({
+    xy: [0, 0],
+    config: i === 0 ? fast : slow
+  }))
+  const [ref, { left, top }] = useMeasure()
 
-  // const handleMouseMove = (e) => {
-  //   api.start({ xy: [e.clientX - left, e.clientY - top] })
-  // }
+  const handleMouseMove = (e) => {
+    api.start({ xy: [e.clientX - left, e.clientY - top] })
+  }
 
   return (
     <MantineProvider
@@ -59,7 +59,7 @@ export default function App({ Component, pageProps }: AppProps) {
       }}
     >
       <Notifications position="bottom-right" />
-      {/* <div className={styles.container}>
+      <div className={styles.container}>
         <svg style={{ position: 'absolute', width: 0, height: 0 }}>
           <filter id="goo">
             <feGaussianBlur
@@ -85,16 +85,16 @@ export default function App({ Component, pageProps }: AppProps) {
             />
           ))}
         </div>
-      </div> */}
+      </div>
       <Box>
-        {/* <AnimatedCursor
+        <AnimatedCursor
           innerSize={15}
           outerSize={15}
           color="253, 186, 116"
           outerAlpha={0.4}
           innerScale={0.7}
           outerScale={5}
-        /> */}
+        />
         <Layout>
           <Component {...pageProps} />
         </Layout>
