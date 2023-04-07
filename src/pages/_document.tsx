f0e3b70e01c157237ceeb63a57ee4d94ee07fb43
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2023-04-08 00:52:13 +0630

    add animate mouse pointer follow

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="SaiHP's Portfolio" content="SaiHP's Portfolio" />
+        <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100;200;300;400;500;600;700;800&display=swap"
           rel="stylesheet"
